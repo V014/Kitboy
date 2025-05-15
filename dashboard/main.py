362320080e4ea3fcec3b82ff1bfca83f760278a6
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-05-15 22:02:09 +0200

    added main theme color from login screen to dashboard

--- dashboard/main.py
+++ dashboard/main.py
@@ -13,7 +13,7 @@ app.resizable(0,0)
 
 # set_appearance_mode("light")
 
-sidebar_frame = CTkFrame(master=app, fg_color="#2A8C55",  width=176, height=650, corner_radius=0)
+sidebar_frame = CTkFrame(master=app, fg_color="#601E88",  width=176, height=650, corner_radius=0)
 sidebar_frame.pack_propagate(0)
 sidebar_frame.pack(fill="y", anchor="w", side="left")
 
@@ -30,7 +30,7 @@ CTkButton(master=sidebar_frame, image=analytics_img, text="Dashboard", fg_color=
 package_img_data = Image.open("package_icon.png")
 package_img = CTkImage(dark_image=package_img_data, light_image=package_img_data)
 
-CTkButton(master=sidebar_frame, image=package_img, text="Orders", fg_color="#fff", font=("Arial Bold", 14), text_color="#2A8C55", hover_color="#eee", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
+CTkButton(master=sidebar_frame, image=package_img, text="Orders", fg_color="#fff", font=("Arial Bold", 14), text_color="#601E88", hover_color="#eee", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
 
 list_img_data = Image.open("list_icon.png")
 list_img = CTkImage(dark_image=list_img_data, light_image=list_img_data)
@@ -55,14 +55,14 @@ main_view.pack(side="left")
 title_frame = CTkFrame(master=main_view, fg_color="transparent")
 title_frame.pack(anchor="n", fill="x",  padx=27, pady=(29, 0))
 
-CTkLabel(master=title_frame, text="Orders", font=("Arial Black", 25), text_color="#2A8C55").pack(anchor="nw", side="left")
+CTkLabel(master=title_frame, text="Orders", font=("Arial Black", 25), text_color="#601E88").pack(anchor="nw", side="left")
 
-CTkButton(master=title_frame, text="+ New Order",  font=("Arial Black", 15), text_color="#fff", fg_color="#2A8C55", hover_color="#207244").pack(anchor="ne", side="right")
+CTkButton(master=title_frame, text="+ New Order",  font=("Arial Black", 15), text_color="#fff", fg_color="#601E88", hover_color="#207244").pack(anchor="ne", side="right")
 
 metrics_frame = CTkFrame(master=main_view, fg_color="transparent")
 metrics_frame.pack(anchor="n", fill="x",  padx=27, pady=(36, 0))
 
-orders_metric = CTkFrame(master=metrics_frame, fg_color="#2A8C55", width=200, height=60)
+orders_metric = CTkFrame(master=metrics_frame, fg_color="#601E88", width=200, height=60)
 orders_metric.grid_propagate(0)
 orders_metric.pack(side="left")
 
@@ -75,7 +75,7 @@ CTkLabel(master=orders_metric, text="Orders", text_color="#fff", font=("Arial Bl
 CTkLabel(master=orders_metric, text="123", text_color="#fff",font=("Arial Black", 15), justify="left").grid(row=1, column=1, sticky="nw", pady=(0,10))
 
 
-shipped_metric = CTkFrame(master=metrics_frame, fg_color="#2A8C55", width=200, height=60)
+shipped_metric = CTkFrame(master=metrics_frame, fg_color="#601E88", width=200, height=60)
 shipped_metric.grid_propagate(0)
 shipped_metric.pack(side="left",expand=True, anchor="center")
 
@@ -87,7 +87,7 @@ CTkLabel(master=shipped_metric, image=shipping_img, text="").grid(row=0, column=
 CTkLabel(master=shipped_metric, text="Shipping", text_color="#fff", font=("Arial Black", 15)).grid(row=0, column=1, sticky="sw")
 CTkLabel(master=shipped_metric, text="91", text_color="#fff",font=("Arial Black", 15), justify="left").grid(row=1, column=1, sticky="nw", pady=(0,10))
 
-delivered_metric = CTkFrame(master=metrics_frame, fg_color="#2A8C55", width=200, height=60)
+delivered_metric = CTkFrame(master=metrics_frame, fg_color="#601E88", width=200, height=60)
 delivered_metric.grid_propagate(0)
 delivered_metric.pack(side="right",)
 
@@ -102,10 +102,10 @@ CTkLabel(master=delivered_metric, text="23", text_color="#fff",font=("Arial Blac
 search_container = CTkFrame(master=main_view, height=50, fg_color="#F0F0F0")
 search_container.pack(fill="x", pady=(45, 0), padx=27)
 
-CTkEntry(master=search_container, width=305, placeholder_text="Search Order", border_color="#2A8C55", border_width=2).pack(side="left", padx=(13, 0), pady=15)
+CTkEntry(master=search_container, width=305, placeholder_text="Search Order", border_color="#601E88", border_width=2).pack(side="left", padx=(13, 0), pady=15)
 
-CTkComboBox(master=search_container, width=125, values=["Date", "Most Recent Order", "Least Recent Order"], button_color="#2A8C55", border_color="#2A8C55", border_width=2, button_hover_color="#207244",dropdown_hover_color="#207244" , dropdown_fg_color="#2A8C55", dropdown_text_color="#fff").pack(side="left", padx=(13, 0), pady=15)
-CTkComboBox(master=search_container, width=125, values=["Status", "Processing", "Confirmed", "Packing", "Shipping", "Delivered", "Cancelled"], button_color="#2A8C55", border_color="#2A8C55", border_width=2, button_hover_color="#207244",dropdown_hover_color="#207244" , dropdown_fg_color="#2A8C55", dropdown_text_color="#fff").pack(side="left", padx=(13, 0), pady=15)
+CTkComboBox(master=search_container, width=125, values=["Date", "Most Recent Order", "Least Recent Order"], button_color="#601E88", border_color="#601E88", border_width=2, button_hover_color="#207244",dropdown_hover_color="#207244" , dropdown_fg_color="#601E88", dropdown_text_color="#fff").pack(side="left", padx=(13, 0), pady=15)
+CTkComboBox(master=search_container, width=125, values=["Status", "Processing", "Confirmed", "Packing", "Shipping", "Delivered", "Cancelled"], button_color="#601E88", border_color="#601E88", border_width=2, button_hover_color="#207244",dropdown_hover_color="#207244" , dropdown_fg_color="#601E88", dropdown_text_color="#fff").pack(side="left", padx=(13, 0), pady=15)
 
 table_data = [
     ["Order ID", "Item Name", "Customer", "Address", "Status", "Quantity"],
@@ -131,8 +131,8 @@ table_data = [
 
 table_frame = CTkScrollableFrame(master=main_view, fg_color="transparent")
 table_frame.pack(expand=True, fill="both", padx=27, pady=21)
-table = CTkTable(master=table_frame, values=table_data, colors=["#E6E6E6", "#EEEEEE"], header_color="#2A8C55", hover_color="#B4B4B4")
-table.edit_row(0, text_color="#fff", hover_color="#2A8C55")
+table = CTkTable(master=table_frame, values=table_data, colors=["#E6E6E6", "#EEEEEE"], header_color="#601E88", hover_color="#B4B4B4")
+table.edit_row(0, text_color="#fff", hover_color="#601E88")
 table.pack(expand=True)
 
 app.mainloop()
