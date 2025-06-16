cca4cb360d198449892df71ae57eaf8cdc188013
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-06-16 22:22:39 +0200

    changed vehicles table header color for better visibility

--- vehicles.py
+++ vehicles.py
@@ -72,7 +72,7 @@ class Vehicles(CTkFrame):
             master=self,
             values=table_display_values,
             colors=["#030712", "#040C15"],
-            header_color="#601E88",
+            header_color="#030712",
             hover_color="#9569AF",
             text_color="#ffffff",
             command=self.handle_table_action  # <-- Add this
