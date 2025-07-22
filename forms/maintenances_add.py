d99f1deb5cac0b65e73116e8f509a31230b46919
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-07-22 15:08:46 +0200

    fixed bug that wasnt allowing the set maintenance page to show

--- forms/maintenances_add.py
+++ forms/maintenances_add.py
@@ -4,22 +4,23 @@ from tkinter import messagebox
 class AddMaintenancesForm(CTkFrame):
     def __init__(self, master, customer_options, vehicle_options, mechanic_options, service_type_options, back_command=None):
         super().__init__(master, fg_color="transparent")
-        self.back_conmmand = back_command
+        self.back_command = back_command
 
-        CTkLabel(self, text="Set Maintenance", font=("Arial Black", 25), text_color="#fff").pack(anchor="nw", pday=(29,0), padx=27)
+        CTkLabel(self, text="Set Maintenance", font=("Arial Black", 25), text_color="#fff").pack(anchor="nw", pady=(29,0), padx=27)
 
         form_frame = CTkFrame(self, fg_color="transparent")
         form_frame.pack(fill="x", padx=27, pady=(10,0))
 
+        
         # Customer selection (ComboBox)
         CTkLabel(form_frame, text="Owner (Customer ID)", font=("Arial Bold", 17), text_color="#fff").grid(row=0, column=0, sticky="w", pady=(0,2))
         self.customer_combo = CTkComboBox(form_frame, values=customer_options, width=300)
         self.customer_combo.grid(row=1, column=0, ipady=10, pady=(0,10))
 
         # Vehicle selection (ComboBox)
-        CTkLabel(form_frame, text="Owner (Vehicle ID)", font=("Arial Bold", 17), text_color="#fff").grid(row=0, column=0, sticky="w", pady=(0,2))
+        CTkLabel(form_frame, text="Owner (Vehicle ID)", font=("Arial Bold", 17), text_color="#fff").grid(row=0, column=1, sticky="w", padx=(25,0), pady=(0,2))
         self.vehicle_combo = CTkComboBox(form_frame, values=vehicle_options, width=300)
-        self.vehicle_combo.grid(row=1, column=0, ipady=10, pady=(0,10))
+        self.vehicle_combo.grid(row=1, column=1, ipady=10, padx=(24,0), pady=(0,10))
 
         # Mechanic selection (ComboBox)
         CTkLabel(form_frame, text="Mechanic ID", font=("Arial Bold", 17), text_color="#fff").grid(row=0, column=0, sticky="w", pady=(0,2))
