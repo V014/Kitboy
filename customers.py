43e4ba92ab032104d56e70699e0f8a2454fff90b
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-07-23 21:51:19 +0200

    adjusted header color in customer table to improve visibility

--- customers.py
+++ customers.py
@@ -72,7 +72,7 @@ class Customers(CTkScrollableFrame):
             master=self,
             values=table_display_values,
             colors=["#030712", "#040C15"],
-            header_color="#030712",
+            header_color="#601E88",
             hover_color="#9569AF",
             text_color="#ffffff",
             command=self.handle_table_action  # <-- Respond to event
