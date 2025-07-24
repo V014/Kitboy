9fb79c62bdf052c834ddffac75f80b8dad8fed01
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-07-24 21:42:28 +0200

    added a utils module to inherit functionallity that is frequently accessed

---
+++ utils.py
@@ -0,0 +1,24 @@
+import connection
+
+class Utils:
+    @staticmethod
+    def get_current_date():
+        from datetime import datetime
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    @staticmethod
+    def get_options(table_name, column_name):
+        db = connection
+        dbcon_func = db.dbcon
+        class DummyDB:
+            pass
+        db_obj = DummyDB()
+        dbcon_func(db_obj)
+        options = []
+        if db_obj.con:
+            try:
+                db_obj.cur.execute(f"SELECT {column_name} FROM {table_name}")
+                options = [str(row[0]) for row in db_obj.cur.fetchall()]
+            finally:
+                db_obj.con.close()
+        return options
